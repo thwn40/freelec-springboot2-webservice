1377f6cfe3a02f0ab7aba1d3112f7a57c7dd1d8a
thwn40 <62760166+thwn40@users.noreply.github.com>
2022-03-01 22:17:34 +0900

    20220301

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -2,8 +2,10 @@ package com.jojoldu.book.springboot;
 import lombok.Getter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
